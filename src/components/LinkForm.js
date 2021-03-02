4cdd123ee298cc8788e8c1cb68aebb2e779bb8e1
Pedro Cobos <pedro.cobos@live.com>
2021-03-01 20:00:31 -0600

    feat: added useState hook to LinkForm component and two event handlers as well

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LinkForm = () => {
+
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("working");
+  }
+
   return (
-    <form className="card card-body">
+    <form className="card card-body" onSubmit={handleSubmit}>
       <div className="form-group input-group">
         <div className="input-group-text bg-light">
-          <i class="material-icons">insert_link</i>
+          <i className="material-icons">insert_link</i>
         </div>
-        <input type="text" className="form-control" name="url" placeholder="url.com"/>
+        <input type="text" className="form-control" name="url" placeholder="url.com" onChange={handleInputChange}/>
       </div>
+
       <div className="form-group input-group">
         <div className="input-group-text bg-light">
-          <i class="material-icons">create</i>
+          <i className="material-icons">create</i>
         </div>
-        <input type="text" className="form-control" name="name" placeholder="Name"/>
+        <input type="text" className="form-control" name="name" placeholder="Name" onChange={handleInputChange}/>
       </div>
+
       <div className="form-group">
-        <textarea name="description" rows="3" className="form-control"></textarea>
+        <textarea name="description" rows="3" className="form-control" placeholder="Write a description" onChange={handleInputChange}></textarea>
       </div>
+
+      <button className="btn btn-primary">
+        Save
+      </button>
     </form>
   )
 }
